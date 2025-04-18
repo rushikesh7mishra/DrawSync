da089725f5d4beafeb783e6fbef7a5444ab37cf1
rushikesh mishra <rushikesh.mishra@walsystems.in>
2025-04-18 17:37:46 +0530

    Added my files to DrawSync

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -23,8 +23,7 @@ const Board = ({ canvasRef,
         const canvas = canvasRef.current;
         canvas.height = window.innerHeight * 2;
         canvas.width = window.innerWidth * 2;
-        // canvas.style.width = `${window.innerWidth}px`;
-        // canvas.style.height = `${window.innerHeight}px`;
+        
         const context = canvas.getContext("2d");
 
         context.strokeWidth = 30;
